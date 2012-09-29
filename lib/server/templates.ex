70c4d116429fbb688f428846dcefd18b787e6e14
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-29 07:55:29 -0700

    Slightly improves package table rendering
    Addresses #9 (incompletely)

--- lib/server/templates.ex
+++ lib/server/templates.ex
@@ -1,3 +1,64 @@
+defmodule Expm.Server.Templates.Package do
+  require EEx
+
+  EEx.function_from_string :def, :description,
+    %b{
+      <%= description %>
+    }, [:description]
+
+  EEx.function_from_string :def, :directories,
+    %b{
+      <%= lc dir inlist directories do %>
+        <span class="label"><%= dir %></span>
+      <% end %>
+    }, [:directories]
+
+  EEx.function_from_string :def, :homepage,
+    %b{
+      <%= if nil?(homepage) do %>
+      <% else %>
+        <a href="<%= homepage %>"><%= homepage %></a>
+      <% end %>
+    }, [:homepage]
+
+  EEx.function_from_string :def, :keywords,
+    %b{
+      <%= lc keyword inlist keywords do %>
+        <span class="label label-info"><%= keyword %></span>
+      <% end %>
+    }, [:keywords]
+
+  EEx.function_from_string :def, :name,
+    %b{
+      <%= name %>
+    }, [:name]
+
+  EEx.function_from_string :def, :version,
+    %b{
+      <%= version %>
+    }, [:version]
+
+  def maintainers(maintainers), do: people(maintainers)
+  def contributors(contributors), do: people(contributors)
+
+  EEx.function_from_string :defp, :people,
+    %b{
+      <%= lc person inlist people do %>
+        <span class="label label-success"><%= person[:name] %> &lt;<%= person[:email] %>&gt;</span>
+      <% end %>
+    }, [:people]
+
+
+  def render(field, value) do
+    if function_exported?(__MODULE__, field, 1) do
+      apply(__MODULE__, field, [value])
+    else
+      inspect value
+    end
+  end
+
+end
+
 defmodule Expm.Server.Templates do
   require EEx
 
@@ -41,7 +102,7 @@ defmodule Expm.Server.Templates do
       <%= lc \{field, value\} inlist package.to_keywords do %>
          <tr>
            <td><b><%= to_binary field %></b></td>
-           <td><%= inspect value %></td>
+           <td><%= Expm.Server.Templates.Package.render(field,value) %></td>
          </tr>
       <% end %>
       </tbody>
