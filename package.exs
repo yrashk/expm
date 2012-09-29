59a3a31f8fa2ae59474ef755dd3e8765e7f13791
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-28 23:06:34 -0700

    Add package.exs file

---
+++ package.exs
@@ -0,0 +1,4 @@
+Expm.Package.new(name: "expm", description: "Elixir package manager",
+                 version: "0.0.1", keywords: ["Elixir","Erlang","package","library","dependency","dependencies"], 
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/expm", tag: "0.0.1"]])
