b301bc0a8f26ef7cc0b8264867ff38954515ee3e
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-28 19:52:17 -0700

    Missing mixfile updates

--- mix.exs
+++ mix.exs
@@ -9,10 +9,19 @@ defmodule Expm.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+      [applications: [:lager, :lagerex, :hackney, :ranch, :cowboy, :genx, :crypto],
+       mod: {Expm, []}]
   end
 
   defp deps do
-    []
+    [
+      {:hackney, %r(.*), github: "benoitc/hackney"},
+      {:mimetypes, %r(.*), github: "spawngrid/mimetypes"},
+      {:edown, %r(.*), github: "esl/edown"},
+      {:genx, %r(.*), github: "yrashk/genx"},
+      {:ranch, %r(.*), github: "extend/ranch"},
+      {:cowboy, %r(.*), github: "extend/cowboy"},
+      {:lagerex, %r(.*), github: "yrashk/lagerex"},
+    ]
   end
 end
