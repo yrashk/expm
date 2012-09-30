f69dd89e77b7807fde42809a0e90185bf069a497
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-30 01:11:09 -0700

    Extract Expm.Package.Decoder to a separate file

--- lib/package.ex
+++ lib/package.ex
@@ -1,39 +1,3 @@
-defmodule Expm.Package.Decoder do
-  defexception SecurityException, message: nil
-  def decode({ :__block__, _, [b] }) when is_list(b) do
-    decode(b)
-  end    
-  def decode(list) when is_list(list) do
-    lc i inlist list, do: decode(i)
-  end  
-  def decode({:access,l1,[{:__aliases__,l2,[:Expm,:Package]}|rest]}) do
-    {:access,l1,[{:__aliases__,l2,[:Expm,:Package]}|decode_1(rest)]}
-  end
-  def decode(v) do
-    raise SecurityException.new(message: "#{Macro.to_binary(v)} is not allowed")  
-  end
-
-  defp decode_1({ :{}, b, c }) do
-    {:{}, b, decode_1(c) }
-  end
-
-  defp decode_1({ _a, _b, _c }=v) do
-    raise SecurityException.new(message: "#{Macro.to_binary(v)} is not allowed")
-  end
-
-  defp decode_1({ a, b }) do
-    { decode_1(a), decode_1(b) }
-  end
-
-  defp decode_1(list) when is_list(list) do
-    lc i inlist list, do: decode_1(i)
-  end
-
-  defp decode_1(other) do
-    other
-  end
-end
-
 defrecord Expm.Package, 
   metadata: [],
   # required
