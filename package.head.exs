f131701cb201d4f3af12ef0f406a3a05e64f2eaa
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-30 10:52:43 -0700

    Add expmified dependencies to package.head.exs

--- package.head.exs
+++ package.head.exs
@@ -1,4 +1,7 @@
 Expm.Package.new(name: "expm", description: "Elixir package manager",
                  version: :head, keywords: ["Elixir","Erlang","package","library","dependency","dependencies"], 
                  maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
-                 repositories: [[github: "yrashk/expm"]])
+                 repositories: [[github: "yrashk/expm"]],
+                 dependencies: [{"validatex", :head},
+                                "mimetypes",
+                                "genx"])
