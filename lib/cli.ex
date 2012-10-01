4a7a9b114d62055de26d28a7b2c4366cdb0452f7
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-01 03:36:13 -0700

    Add some CLI help information

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,27 +1,99 @@
+defmodule Expm.CLI.Command do
+  defmacro __using__(_) do
+    quote do
+      import Expm.CLI.Command
+      Module.register_attribute __MODULE__, :command
+    end
+  end
+  defmacro command(pattern, arg, body) do
+    command = if pattern == [], do: "<no command>", else: hd(pattern)
+    quote do
+      @command {unquote(command), @doc, @shortdoc}
+      Module.delete_attribute __MODULE__, :doc
+      Module.delete_attribute __MODULE__, :shortdoc
+      def(run(unquote(pattern), unquote(arg)), unquote(body))
+    end
+  end
+end
 defrecord Expm.CLI, repository: Expm.Repository.HTTP.new.url, username: nil, password: nil,
                     version: false, format: "asis", format_opts: [] do
 
-  def run([], rec) do
+  use Expm.CLI.Command
+
+  repository_switch_doc = """
+  Options:
+
+    --repository URL (-r URL)
+
+  By default, this is [http://expm.co/](http://expm.co/)
+  """
+  auth_switch_doc = """
+    --username USER and --password PASSWORD
+
+  Used when publishing a package to the repository    
+  """
+
+  @shortdoc :skip
+  @doc """
+  $ expm
+
+  Prints out software information
+
+  Currently supported options: --version
+
+  Example:
+    expm --version  
+  """
+  command([], rec) do
     cond do
       rec.version == true ->
         IO.puts Expm.version
       true ->
-        run(nil, rec)
+        run(["help"], rec)
     end
   end
+ 
+  @shortdoc "Server information"
+  @doc """
+  $ expm server
 
-  def run(["server"], rec) do
+  Prints server information
+
+  Currently supported options: --version
+
+  Example:
+    expm server --version
+  
+  #{repository_switch_doc}
+  """
+  command(["server"], rec) do
     cond do
       rec.version == true ->
         IO.puts repo(rec).version
     end
   end
 
-  def run(["list"], rec) do
+  @shortdoc "List packages"
+  @doc """
+  $ expm list
+
+  Lists all packages in the repository
+
+  #{repository_switch_doc}
+  """
+  command(["list"], rec) do
     run(["search", ""], rec)
   end
 
-  def run(["search", kwd], rec) do
+  @shortdoc "Search packages"
+  @doc """
+  $ expm search KEYWORD
+
+  Searches all packages that match the KEYWORD. KEYWORD can be a partial regular expression.
+
+  #{repository_switch_doc}
+  """
+  command(["search", kwd], rec) do
     repo = repo(rec)
     pkgs = Expm.Package.search repo, kwd
     lc pkg inlist pkgs do
@@ -29,6 +101,24 @@ defrecord Expm.CLI, repository: Expm.Repository.HTTP.new.url, username: nil, pas
     end
   end
 
+  @shortdoc "Show package specification"
+  @doc """
+  $ expm spec[:FIELD] PACKAGE [--format mix|rebar|asis] [--format-opts OPTS]
+
+      Prints out topmost PACKAGE's version's specification.
+      If FIELD is specified, only that field is printed.
+
+  $ expm spec[:FIELD] PACKAGE VERSION [--format mix|rebar|asis] [--format-opts OPTS]
+
+      Prints out specific PACKAGE's VERSION's specification.
+
+      If FIELD is specified, only that field is printed.
+
+  #{repository_switch_doc}
+  """
+  command(["spec", _package], rec) do
+    run(["help","spec"], rec)
+  end
   def run([<<"spec", field :: binary>>, package], rec) do
     repo = repo(rec)
     pkg = Expm.Package.fetch repo, package
@@ -51,24 +141,72 @@ defrecord Expm.CLI, repository: Expm.Repository.HTTP.new.url, username: nil, pas
     end
   end
 
-  def run(["versions", package], rec) do
+  @shortdoc "Package versions"
+  @doc """
+  $ expm versions
+  Prints a list of PACKAGE's versions
+
+  #{repository_switch_doc}
+  """
+  command(["versions", package], rec) do
     repo = repo(rec)
     lc version inlist Expm.Package.versions(repo, package) do
       IO.puts version
     end
   end
 
-  def run(["publish"], rec) do
+  @shortdoc "Publish a package"
+  @doc """
+  $ expm publish
+ 
+  Publishes package.exs to the repository
+ 
+  #{repository_switch_doc}
+  #{auth_switch_doc}
+  """
+  command(["publish"], rec) do
     run(["publish", "package.exs"], rec)
   end
 
-  def run(["publish", file], rec) do
+  @shortdoc :skip
+  @doc """
+  $ expm publish [filename.exs]
+
+  Publishes filename.exs to the repository
+ 
+  #{repository_switch_doc}
+  #{auth_switch_doc}  
+  """
+  command(["publish", file], rec) do
     pkg = Expm.Package.read(file)
     IO.inspect pkg.publish(repo(rec))
   end
+  
+  @shortdoc :skip
+  @doc """
+  $ expm help [command]
 
-  def run(_, _) do
-    IO.puts "Invalid command"
+  Prints documentation on a specific command
+  """
+  command(["help", command], _rec) do
+    lc {:command, [{cmd, doc, _}]} inlist __info__(:attributes), cmd == command do
+      IO.puts doc || "No help available"
+    end    
+  end
+
+  @shortdoc "Help"
+  @doc """
+  $ expm help
+
+  Prints help information
+  """
+  command(["help"], _rec) do
+    IO.puts "Commands:\n"
+    lc {:command, [{cmd, _, shortdoc}]} inlist __info__(:attributes) do
+      unless shortdoc == :skip do
+        :io.format("~-20ts ~ts~n",[cmd, shortdoc || ""])
+      end
+    end
   end
 
   defp spec_field(pkg, "", rec), do: do_format(pkg, rec)
