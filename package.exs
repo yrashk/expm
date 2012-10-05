9774417a3f366c99c21db05c221ec7ea730f276d
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-05 11:56:29 -0700

    Bump up to 0.1

--- package.exs
+++ package.exs
@@ -1,4 +1,4 @@
 Expm.Package.new(name: "expm", description: "Elixir package manager",
-                 version: "0.0.2", keywords: ["Elixir","Erlang","package","library","dependency","dependencies"], 
+                 version: "0.1", keywords: ["Elixir","Erlang","package","library","dependency","dependencies"], 
                  maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
-                 repositories: [[github: "yrashk/expm", tag: "0.0.2"]])
+                 repositories: [[github: "yrashk/expm", tag: "0.1"]])
