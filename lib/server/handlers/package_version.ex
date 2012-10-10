268b0d28afd53f296a0f4625a659fc76e87faade
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-10 14:38:59 -0700

    Don't lose the package name and version in the hero unit when displaying /package/version

--- lib/server/handlers/package_version.ex
+++ lib/server/handlers/package_version.ex
@@ -15,7 +15,10 @@ defmodule Expm.Server.Http.PackageVersion do
   end
 
   def to_html(req, State[repository: repository, package: pkg] = state) do
-    out = Expm.Server.Http.render_page(Expm.Server.Templates.package(pkg, repository), req, state)
+    tpl = Expm.Server.Templates.package(pkg, repository)
+    out = Expm.Server.Http.render_page(tpl, req, state,
+                                       title: "#{pkg.name} (#{pkg.version})", 
+                                       subtitle: pkg.description)
     {out, req, state}
   end
 
