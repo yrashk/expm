b6acb3247e2cfadf11c10dcc989449478a8e91d9
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-14 08:41:42 -0700

    Add release building functionality

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: expm
 
-.PHONY: ebin
+.PHONY: ebin rel
 
 ebin:
 	@mix do deps.get, compile
@@ -9,6 +9,16 @@ expm: ebin
 	@mix escriptize
 	@cp ./expm priv/static
 
-start: expm
+sys.config: sys.config.exs
 	@mix run "{:ok, b} = File.read \"sys.config.exs\"; {v, _} = Code.eval(b); :io.format(\"~p.~n\", [v])" > sys.config
+
+start: expm sys.config
 	@ERL_LIBS=deps elixir --sname expm --erl "-pa ebin -config sys -s Elixir-Expm" --no-halt
+
+rel: rel/expm
+
+rel/expm: expm
+	@mix relex.assemble
+
+start-rel: rel sys.config
+	@./rel/expm/erts-*/bin/erl -sname expm -config sys -s 'Elixir-Expm' -noinput
--- mix.exs
+++ mix.exs
@@ -1,3 +1,5 @@
+Code.append_path "deps/relex/ebin"
+
 defmodule Expm.Mixfile do
   use Mix.Project
 
@@ -5,13 +7,15 @@ defmodule Expm.Mixfile do
     [ app: :expm,
       version: String.strip(System.cmd("git describe --always --tags"),?\n),
       deps: deps,
-      escript_embed_elixir: true
+      escript_embed_elixir: true,
+      release_options: [path: "rel"]
     ]
   end
 
   # Configuration for the OTP application
   def application do
-      [applications: [:lager, :lagerex, :hackney, :ranch, :cowboy, :genx, :crypto, :exreloader, :erlpass],
+      [applications: [:lager, :lagerex, :hackney, :ranch, :cowboy, :mimetypes, :inets,
+                      :genx, :crypto, :exreloader, :erlpass],
        mod: {Expm, []}]
   end
 
@@ -26,9 +30,21 @@ defmodule Expm.Mixfile do
       {:cowboy, %r(.*), github: "extend/cowboy"},
       {:lagerex, %r(.*), github: "yrashk/lagerex"},
       {:exreloader, %r(.*), github: "yrashk/exreloader"},
-      {:erlpass, %r(.*), github: "ferd/erlpass", compile: "rebar compile deps_dir=.."},
+      {:erlpass, %r(.*), github: "yrashk/erlpass", branch: "patch-1", compile: "rebar compile deps_dir=.."},
         {:proper, %r(.*),github: "manopapad/proper"},
         {:bcrypt, %r(.*), github: "spawngrid/erlang-bcrypt"},
+      {:relex, github: "yrashk/relex"},
     ]
+  end
+
+  if Code.ensure_loaded?(Relex.Release) do
+    defmodule Release do
+      use Relex.Release
+
+      def name, do: "expm"
+      def version, do: Mix.project[:version]
+      def applications, do: [:expm]
+      def lib_dirs, do: ["deps"]
+    end
   end
 end
